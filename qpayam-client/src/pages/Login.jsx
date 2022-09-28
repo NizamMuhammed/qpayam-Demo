e38c587ea93f1e3cceb6ad7580846c55bf41d631
NizamMuhammed <mohammednizampn@gmail.com>
2022-09-28 23:01:18 +0530

    implemented link navigation using react-route-dom

--- qpayam-client/src/pages/Login.jsx
+++ qpayam-client/src/pages/Login.jsx
@@ -1,13 +1,9 @@
-import Header from "../components/Header"
-import Footer from "../components/Footer"
 import React from "react"
 
 const Login = () => {
   return (
     <div>
-      <Header />
       <div style={{ padding: "2rem" }}>Login Page</div>
-      <Footer />
     </div>
   )
 }
--- qpayam-client/src/pages/Register.jsx
+++ qpayam-client/src/pages/Register.jsx
@@ -1,13 +1,9 @@
-import Header from "../components/Header"
-import Footer from "../components/Footer"
 import React from "react"
 
 const Register = () => {
   return (
     <div>
-      <Header />
       <div style={{ padding: "2rem" }}>Register Page</div>
-      <Footer />
     </div>
   )
 }
