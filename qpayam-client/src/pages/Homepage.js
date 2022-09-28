a487aecd9f264dee0e3d75c42b28633e4b00655f
NizamMuhammed <mohammednizampn@gmail.com>
2022-09-28 21:10:07 +0530

    created header footer and pages

---
+++ qpayam-client/src/pages/Homepage.js
@@ -0,0 +1,15 @@
+import Header from "../components/Header"
+import Footer from "../components/Footer"
+import React from "react"
+
+const Homepage = () => {
+  return (
+    <div>
+      <Header />
+      <div style={{ padding: "2rem" }}>Hello World</div>
+      <Footer />
+    </div>
+  )
+}
+
+export default Homepage
---
+++ qpayam-client/src/pages/Register.js
@@ -0,0 +1,15 @@
+import Header from "../components/Header"
+import Footer from "../components/Footer"
+import React from "react"
+
+const Register = () => {
+  return (
+    <div>
+      <Header />
+      <div style={{ padding: "2rem" }}>Register Page</div>
+      <Footer />
+    </div>
+  )
+}
+
+export default Register
