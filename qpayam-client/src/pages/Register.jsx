1b209d23ac5c5e05c0d94dcf88382e8975809c5a
NizamMuhammed <mohammednizampn@gmail.com>
2022-10-01 23:12:44 +0530

    made register page

--- qpayam-client/src/pages/Register.jsx
+++ qpayam-client/src/pages/Register.jsx
@@ -1,9 +1,63 @@
-import React from "react"
+import React, { useState } from "react"
+import "../styles/Register.css"
 
 const Register = () => {
+  const [stateValues, setStateValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    buttonText: "Register",
+    error: "",
+    success: "",
+  })
+
+  const handleChange = (value) => (e) => {
+    setStateValues({
+      ...stateValues,
+      [value]: e.target.value,
+      error: "",
+      success: "",
+    })
+  }
+
   return (
-    <div>
-      <div style={{ padding: "2rem" }}>Register Page</div>
+    <div className="mainContainer">
+      <div className="backgroundContainer">
+        <form className="formContainer">
+          <h1 className="signupTitle">Signup</h1>
+          <div className="inputMainContainer">
+            <div className="inputContainer">
+              <h3 className="inputText">Name</h3>
+              <input
+                placeholder="Enter your Name"
+                className="inputField"
+                type="text"
+                name="name"
+                onChange={handleChange}
+              ></input>
+            </div>
+            <div className="inputContainer">
+              <h3 className="inputText">Email</h3>
+              <input
+                placeholder="Enter your Email"
+                className="inputField"
+                onChange={handleChange}
+              ></input>
+            </div>
+            <div className="inputContainer">
+              <h3 className="inputText">Password</h3>
+              <input
+                placeholder="Enter your Password"
+                className="inputField"
+                onChange={handleChange}
+              ></input>
+            </div>
+          </div>
+          <button type="submit" className="submitButton">
+            {stateValues.buttonText}
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
