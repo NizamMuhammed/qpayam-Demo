3e217da89c608e2b56cf80dd2261227d2cedd4b3
NizamMuhammed <mohammednizampn@gmail.com>
2022-10-02 21:59:10 +0530

    created user schema model

--- qpayam-server/models/user.js
+++ qpayam-server/models/user.js
@@ -11,27 +11,43 @@ import mongoose from "mongoose"
  * index: for searching and indexing purposes
  */
 
-const userSchema = mongoose.Schema({
-  username: {
-    type: String,
-    trim: true,
-    required: true,
-    max: 20,
-    unique: true,
-    index: true,
-    lowercase: true,
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      required: true,
+      max: 20,
+      unique: true,
+      index: true,
+      lowercase: true,
+    },
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      max: 32,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    hashedPassword: {
+      type: String,
+      required: true,
+    },
+    salt: String,
+    role: {
+      type: String,
+      default: "subscriber",
+    },
+    resetPasswordLink: {
+      data: String,
+      default: "",
+    },
   },
-  name: {
-    type: String,
-    trim: true,
-    required: true,
-    max: 32,
-  },
-  email: {
-    type: String,
-    trim: true,
-    required: true,
-    unique: true,
-    lowercase: true,
-  },
-})
+  { timestamps: true }
+)
