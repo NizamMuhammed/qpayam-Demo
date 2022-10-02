a90fe18f787ffda8eccec7ff8edd0107eb4bb96d
NizamMuhammed <mohammednizampn@gmail.com>
2022-10-02 20:59:09 +0530

    implement  controllers and mongoose

---
+++ qpayam-server/controllers/auth.js
@@ -0,0 +1,9 @@
+//Controllers are used for code cluttering
+
+const register = (req, res) => {
+  res.json({
+    data: "You just hit the Register endpoint middleware",
+  })
+}
+
+export { register }
---
+++ qpayam-server/models/user.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose"
+
+/**
+ * Schema configuration
+ *
+ * type - variable type
+ * trim - to remove spaces
+ * required - necessary for validation
+ * max - length of variable value
+ * unique - no same usernames
+ * index: for searching and indexing purposes
+ */
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    trim: true,
+    required: true,
+    max: 20,
+    unique: true,
+    index: true,
+    lowercase: true,
+  },
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+    max: 32,
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+})
