b9c9abc3126c83a98a590c7ad3b2b3157a861240
NizamMuhammed <mohammednizampn@gmail.com>
2022-10-03 07:05:58 +0530

    minor name changes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,4 +5,4 @@ const routes = express.Router()
 
 routes.get("/register", register) //The register function is a controller
 
-export default routes
+export { routes }
--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 import express from "express"
-import routes from "./routes/auth.js" //import Auth routes
+import { routes as authRoute } from "./routes/auth.js" //import Auth routes
 import morgan from "morgan"
 import bodyParser from "body-parser"
 import cors from "cors"
@@ -16,7 +16,7 @@ const app = express()
 
 //Middlewares
 
-app.use("/api", routes) //endpoint starts with '/api/
+app.use("/api", authRoute) //endpoint starts with '/api/
 app.use(morgan("dev"))
 app.use(bodyParser.json())
 app.use(cors())
